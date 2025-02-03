e66ff0d3d1497e5e2e566713d8369a9033be7b63
ChlukasX <lukasnackmayr@proton.me>
2025-02-03 18:24:27 +0100

    feat: cleaned up the structure, less responsibility in main with routes. Added custom error logs

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,14 +3,13 @@ package main
 import (
 "fmt"
 "html/template"
-"log"
 "net/http"
 "strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
   if r.URL.Path != "/" {
-    http.NotFound(w, r)
+    app.notFound(w)
     return
   }
 
@@ -22,36 +21,32 @@ func home(w http.ResponseWriter, r *http.Request) {
 
   ts, err := template.ParseFiles(files...)
   if err != nil {
-    log.Print(err.Error())
-    http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+    app.serverError(w, err)
     return
   }
 
   err = ts.ExecuteTemplate(w, "base", nil)
   if err != nil {
-    log.Print(err.Error())
-    http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+    app.errorLog.Print(err.Error())
+    app.serverError(w, err)
     return
   }
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
   id, err := strconv.Atoi(r.URL.Query().Get("id"))
   if err != nil || id < 1 {
-    http.NotFound(w, r)
+    app.notFound(w)
     return
   }
 
   fmt.Fprintf(w, "Display a specific snippet with ID %d", id)
 }
 
-func snipperCreate(w http.ResponseWriter, r *http.Request) {
+func (app *application) snipperCreate(w http.ResponseWriter, r *http.Request) {
   if r.Method != http.MethodPost {
     w.Header().Set("Allow", http.MethodPost)
-//    w.WriteHeader(405)
-//    w.Write([]byte("Method not allowed"))
-//    http.Error is a helper function that combineds w.Write and w.WriteHeader
-    http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+    app.clientError(w, http.StatusMethodNotAllowed)
     return
   }
   w.Write([]byte("Create a specific snippet"))
