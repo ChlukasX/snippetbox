602a20a6cb035dcb6fe4d6bccbdaef340ee175b6
ChlukasX <lukasnackmayr@proton.me>
2025-02-15 17:39:25 +0100

    feat: added helper to decode forms for easier form handling using the go-playground library

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,102 +7,91 @@ import (
 	"strconv"
 
 	"snippetbox.chlukas.github/internal/models"
-  "snippetbox.chlukas.github/internal/validator"
+	"snippetbox.chlukas.github/internal/validator"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-  snippets, err := app.snippets.Latest()
-  if err != nil {
-    app.serverError(w, err)
-    return
-  }
+	snippets, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
 
-  data := app.newTemplateData(r)
-  data.Snippets = snippets
+	data := app.newTemplateData(r)
+	data.Snippets = snippets
 
-  app.render(w, http.StatusOK, "home.tmpl.html", data)
+	app.render(w, http.StatusOK, "home.tmpl.html", data)
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-  params := httprouter.ParamsFromContext(r.Context())
+	params := httprouter.ParamsFromContext(r.Context())
 
-  id, err := strconv.Atoi(params.ByName("id"))
-  if err != nil || id < 1 {
-    app.notFound(w)
-    return
-  }
+	id, err := strconv.Atoi(params.ByName("id"))
+	if err != nil || id < 1 {
+		app.notFound(w)
+		return
+	}
 
-  snippet, err := app.snippets.Get(id)
-  if err != nil {
-    if errors.Is(err, models.ErrNoRecord) {
-      app.notFound(w)
-    } else { 
-      app.serverError(w, err)
-    }
-    return 
-  }
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
 
-  data := app.newTemplateData(r)
-  data.Snippet = snippet
+	data := app.newTemplateData(r)
+	data.Snippet = snippet
 
-  app.render(w, http.StatusOK, "view.tmpl.html", data)
+	app.render(w, http.StatusOK, "view.tmpl.html", data)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-  data := app.newTemplateData(r)
-  data.Form = snippetCreateForm{
-    Expires: 365,
-  }
+	data := app.newTemplateData(r)
+	data.Form = snippetCreateForm{
+		Expires: 365,
+	}
 
-  app.render(w, http.StatusOK, "create.tmpl.html", data)
+	app.render(w, http.StatusOK, "create.tmpl.html", data)
 }
 
 type snippetCreateForm struct {
-  Title string
-  Content string
-  Expires int
-  validator.Validator
+	Title               string `form:"title"`
+	Content             string `form:"content"`
+	Expires             int    `form:"expires"`
+	validator.Validator `form:"-"`
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-  err := r.ParseForm()
-  if err != nil {
-    app.clientError(w, http.StatusBadRequest)
-    return
-  }
+  var form snippetCreateForm
 
-  expires, err := strconv.Atoi(r.PostForm.Get("expires"))
-  if err != nil {
-    app.clientError(w, http.StatusBadRequest)
-    return
-  }
+	err := app.decodePostForm(r, &form)
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
 
-  form := snippetCreateForm{
-    Title: r.PostForm.Get("title"),
-    Content: r.PostForm.Get("content"),
-    Expires: expires,
-  }
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
+	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
+	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
 
-  form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
-  form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
-  form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
-  form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
+	if !form.Valid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, http.StatusUnprocessableEntity, "create.tmpl.html", data)
+		return
+	}
 
-  if !form.Valid() {
-    data := app.newTemplateData(r)
-    data.Form = form
-    app.render(w, http.StatusUnprocessableEntity, "create.tmpl.html", data)
-    return
-  }
+	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
 
-  id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
-  if err != nil {
-    app.serverError(w, err)
-    return
-  }
-
-  http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
-
