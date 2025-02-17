132cd2d68b6dbdf025b2680e6bed36d0b4121566
ChlukasX <lukasnackmayr@proton.me>
2025-02-17 15:12:30 +0100

    feat: added signup, login, and logout with validation and flash

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -124,37 +124,99 @@ func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
 	form.CheckField(validator.NotBlank(form.Password), "password", "This field cannot be blank")
 	form.CheckField(validator.MinChars(form.Password, 8), "password", "This field must be at least 8 characters long")
 
-  if !form.Valid() {
-    data := app.newTemplateData(r)
-    data.Form = form
-    app.render(w, http.StatusUnprocessableEntity, "signup.tmpl.html", data)
-    return
-  }
-  
-  err = app.users.Insert(form.Name, form.Email, form.Password)
+	if !form.Valid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, http.StatusUnprocessableEntity, "signup.tmpl.html", data)
+		return
+	}
+
+	err = app.users.Insert(form.Name, form.Email, form.Password)
+	if err != nil {
+		if errors.Is(err, models.ErrDuplicateEmail) {
+			form.AddFieldError("email", "Email address is already in use")
+
+			data := app.newTemplateData(r)
+			data.Form = form
+			app.render(w, http.StatusUnprocessableEntity, "signup.tmpl.html", data)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	app.sessionManager.Put(r.Context(), "flash", "Your signup was succesfull. Please log in.")
+
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+}
+
+type userLoginForm struct {
+	Email               string `form:"email"`
+	Password            string `form:"password"`
+	validator.Validator `form:"-"`
+}
+
+func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
+	data := app.newTemplateData(r)
+	data.Form = userLoginForm{}
+	app.render(w, http.StatusOK, "login.tmpl.html", data)
+}
+
+func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
+	var form userLoginForm
+
+	err := app.decodePostForm(r, &form)
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	form.CheckField(validator.NotBlank(form.Email), "email", "This field cannot be blank")
+	form.CheckField(validator.Matches(form.Email, validator.EmailRX), "email", "This field must be a valid email address")
+	form.CheckField(validator.NotBlank(form.Password), "password", "This field cannot be blank")
+
+	if !form.Valid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, http.StatusUnprocessableEntity, "login.tmpl.html", data)
+		return
+	}
+
+  id, err := app.users.Authenticate(form.Email, form.Password)
   if err != nil {
-    if errors.Is(err, models.ErrDuplicateEmail) {
-      form.AddFieldError("email", "Email address is already in use")
+    if errors.Is(err, models.ErrInvalidCredentials) {
+      form.AddNonFieldError("Email or password incorrect")
 
       data := app.newTemplateData(r)
       data.Form = form
-      app.render(w, http.StatusUnprocessableEntity, "signup.tmpl.html", data)
+      app.render(w, http.StatusUnprocessableEntity, "login.tmpl.html", data)
     } else {
       app.serverError(w, err)
     }
     return
   }
 
-  app.sessionManager.Put(r.Context(), "flash", "Your signup was succesfull. Please log in.")
+  err = app.sessionManager.RenewToken(r.Context())
+  if err != nil {
+    app.serverError(w, err)
+    return
+  }
 
-  http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+  app.sessionManager.Put(r.Context(), "authenticatedUserID", id)
+
+  http.Redirect(w, r, "/snippet/create", http.StatusSeeOther)
 }
-func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Display a HTML form for logging in a user...")
-}
-func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Authenticate and login the user...")
-}
+
 func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Logout the user...")
+  err := app.sessionManager.RenewToken(r.Context())
+  if err != nil {
+    app.serverError(w, err)
+    return
+  }
+
+  app.sessionManager.Remove(r.Context(), "authenticatedUserID")
+
+  app.sessionManager.Put(r.Context(), "flash", "You've been logged out successfully!")
+	
+  http.Redirect(w, r, "/", http.StatusSeeOther)
 }
